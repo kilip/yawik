3140f96166a2a1a183a9ecac58d587198bdef4c6
Anthonius Munthi <me@itstoni.com>
2021-09-05 10:12:49 +0800

    cs-fix

--- api/src/Resource/Doctrine/Client.php
+++ api/src/Resource/Doctrine/Client.php
@@ -1,5 +1,16 @@
 <?php
 
+/*
+ * This file is part of the Yawik project.
+ *
+ * (c) 2013-2021 Cross Solution
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
 namespace Yawik\Resource\Doctrine;
 
 use MongoDB\Client as BaseClient;
@@ -8,7 +19,7 @@ class Client extends BaseClient
 {
     public function __construct(string $uri = 'mongodb://127.0.0.1/', array $uriOptions = [], array $driverOptions = [])
     {
-        if(false !== str_contains($uri, 'mongodb+srv')){
+        if (false !== str_contains($uri, 'mongodb+srv')) {
             $uriOptions = [];
         }
         parent::__construct($uri, $uriOptions, $driverOptions);
